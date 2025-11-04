ac437c9d077236bfcffb51f83a62598cc4afd567
bearyong-dev <bearyong.dev@gmail.com>
2025-11-04 15:25:37 +0700

    commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hello Rbac";
+echo "Welcome to the Menu Page";
+echo "This is the menu page of our website";
+?>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Home";
+    echo "Menu:";
+    echo "Home";
+?>
